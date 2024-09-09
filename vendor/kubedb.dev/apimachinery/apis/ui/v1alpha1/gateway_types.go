656d8beb46a14c0489bd8f5d1e92ee89a566e6d1
Arnob kumar saha <arnob@appscode.com>
2024-09-09 19:08:48 +0600

    Move Gateway api to kubedb/apimachinery
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

---
+++ vendor/kubedb.dev/apimachinery/apis/ui/v1alpha1/gateway_types.go
@@ -0,0 +1,60 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	core "k8s.io/api/core/v1"
+	ofst "kmodules.xyz/offshoot-api/api/v1"
+)
+
+type Gateway struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+	// +optional
+	IP string `json:"ip,omitempty"`
+	// +optional
+	Hostname string `json:"hostname,omitempty"`
+	// Services is an optional configuration for services used to expose database
+	// +optional
+	Services []NamedServiceStatus `json:"services,omitempty"`
+	// UI is an optional list of database web uis
+	// +optional
+	UI []NamedURL `json:"ui,omitempty"`
+}
+
+type NamedServiceStatus struct {
+	// Alias represents the identifier of the service.
+	Alias string `json:"alias"`
+
+	Ports []ofst.GatewayPort `json:"ports"`
+}
+
+type NamedURL struct {
+	// Alias represents the identifier of the service.
+	// This should match the db ui chart name
+	Alias string `json:"alias"`
+
+	// URL of the database ui
+	URL string `json:"url"`
+
+	Port ofst.GatewayPort `json:"port"`
+
+	// HelmRelease is the name of the helm release used to deploy this ui
+	// The name format is typically <alias>-<db-name>
+	// +optional
+	HelmRelease *core.LocalObjectReference `json:"helmRelease,omitempty"`
+}
