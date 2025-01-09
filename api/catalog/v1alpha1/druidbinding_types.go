0ed162b1037a648ea3d0e3407d8815b8f2f1d89b
M Obaydullah <52800314+obaydullahmhs@users.noreply.github.com>
2025-01-09 12:15:21 +0600

    Add binding interface for generic bindings (#20)
    Signed-off-by: obaydullahmhs <obaydullah@appscode.com>

--- api/catalog/v1alpha1/druidbinding_types.go
+++ api/catalog/v1alpha1/druidbinding_types.go
@@ -63,3 +63,21 @@ type DruidBindingList struct {
 func init() {
 	SchemeBuilder.Register(&DruidBinding{}, &DruidBindingList{})
 }
+
+var _ BindingInterface = &DruidBinding{}
+
+func (in *DruidBinding) GetSourceRef() kmapi.ObjectReference {
+	return in.Spec.SourceRef
+}
+
+func (in *DruidBinding) GetStatus() *BindingStatus {
+	return &in.Status
+}
+
+func (in *DruidBinding) GetConditions() kmapi.Conditions {
+	return in.Status.Conditions
+}
+
+func (in *DruidBinding) SetConditions(conditions kmapi.Conditions) {
+	in.Status.Conditions = conditions
+}
