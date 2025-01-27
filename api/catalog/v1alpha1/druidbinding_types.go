8e916bc34f294dee3c1352e0840f58bacb9a5057
Arnob Kumar Saha <arnob@appscode.com>
2025-01-27 15:00:16 +0600

    Make the binding spec common (#22)
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- api/catalog/v1alpha1/druidbinding_types.go
+++ api/catalog/v1alpha1/druidbinding_types.go
@@ -27,12 +27,6 @@ const (
 	ResourceDruidBindings    = "druidbindings"
 )
 
-// DruidBindingSpec defines the desired state of DruidBinding
-type DruidBindingSpec struct {
-	// SourceRef refers to the source app instance.
-	SourceRef kmapi.ObjectReference `json:"sourceRef"`
-}
-
 // +kubebuilder:object:root=true
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:subresource:status
@@ -47,8 +41,8 @@ type DruidBinding struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   DruidBindingSpec `json:"spec,omitempty"`
-	Status BindingStatus    `json:"status,omitempty"`
+	Spec   BindingSpec   `json:"spec,omitempty"`
+	Status BindingStatus `json:"status,omitempty"`
 }
 
 //+kubebuilder:object:root=true
